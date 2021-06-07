6c5542735579360a065cce614c5bb97d88419053
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2021-06-07 20:11:52 +0200

    Format code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use tract_onnx::prelude::*;
 use clap::{AppSettings, Clap};
 use std::error::Error;
+use tract_onnx::prelude::*;
 
 #[derive(Clap, Debug)]
 struct Image {
@@ -30,9 +30,8 @@ struct Opts {
     subcmd: SubCommand,
 }
 
-fn inspect_model(opts: Opts) -> Result<(), Box<dyn Error>>  {
-    let model = tract_onnx::onnx()
-        .model_for_path(opts.model_path)?;
+fn inspect_model(opts: Opts) -> Result<(), Box<dyn Error>> {
+    let model = tract_onnx::onnx().model_for_path(opts.model_path)?;
 
     for name in model.node_names() {
         println!("{:?}", name);
@@ -45,41 +44,45 @@ fn embed(opts: &Opts, image_opt: &Image) -> Result<(), Box<dyn Error>> {
     let image_size = image_opt.image_size;
     let model = tract_onnx::onnx()
         .model_for_path(opts.model_path.clone())?
-        .with_input_fact(0, InferenceFact::dt_shape(f32::datum_type(), tvec!(1, 3, image_size, image_size)))?
-        .with_output_names(vec!(image_opt.layer_name.clone()))?
+        .with_input_fact(
+            0,
+            InferenceFact::dt_shape(f32::datum_type(), tvec!(1, 3, image_size, image_size)),
+        )?
+        .with_output_names(vec![image_opt.layer_name.clone()])?
         .into_optimized()?
         .into_runnable()?;
 
-
     let image = image::open(&image_opt.image_path).unwrap().to_rgb8();
-    let resized =
-        image::imageops::resize(&image, image_size as u32, image_size as u32, ::image::imageops::FilterType::Triangle);
+    let resized = image::imageops::resize(
+        &image,
+        image_size as u32,
+        image_size as u32,
+        ::image::imageops::FilterType::Triangle,
+    );
 
     let image: Tensor = if image_opt.normalize {
         tract_ndarray::Array4::from_shape_fn((1, 3, image_size, image_size), |(_, c, y, x)| {
             let mean = [0.485, 0.456, 0.406][c];
             let std = [0.229, 0.224, 0.225][c];
-            (resized[(x as _, y as _)][c] as f32 / 255.0 - mean) / std})
-            .into()
+            (resized[(x as _, y as _)][c] as f32 / 255.0 - mean) / std
+        })
+        .into()
     } else {
         tract_ndarray::Array4::from_shape_fn((1, 3, image_size, image_size), |(_, c, y, x)| {
             resized[(x as _, y as _)][c] as f32
-        }).into()
+        })
+        .into()
     };
 
     // run the model on the input
     let result = model.run(tvec!(image))?;
-    let best: Vec<_> = result[0]
-        .to_array_view::<f32>()?
-        .iter()
-        .cloned()
-        .collect();
+    let best: Vec<_> = result[0].to_array_view::<f32>()?.iter().cloned().collect();
 
     println!("{:?}", best);
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     let opts: Opts = Opts::parse();
     match &opts.subcmd {
         SubCommand::Inspect => inspect_model(opts),
