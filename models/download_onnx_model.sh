bfeb59078cd4a6bee7da42e28254f0c63a65bd4f
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2021-06-09 23:58:13 +0200

    Remove download script

--- models/download_onnx_model.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/onnx/models/raw/master/vision/classification/mobilenet/model/mobilenetv2-7.onnx
-wget https://github.com/onnx/models/raw/master/vision/classification/resnet/model/resnet152-v2-7.onnx
-wget https://github.com/onnx/models/raw/master/vision/classification/efficientnet-lite4/model/efficientnet-lite4-11.onnx
